37149e9f2204a48df4d4cf3c586f4221e6ef505d
Suraj <surajyadu.9@gmail.com>
2026-04-14 18:02:32 +0530

    fix hotel api final

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1878207491"
+    serviceWorkerVersion: "3306561638"
   }
 });
